cbd009b274dc5ca0e7314052c901d15dd8b17c3e
CherifiMi <cherifi154949@gmail.com>
2023-08-22 05:43:24 +0100

    .

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,10 @@
 fun main() {
 
 }
+open class StarTrekRepo{
+    private val starShip = mutableMapOf("USS" to "Jean")
+
+    open fun getCaptin(shipName: String): String{
+        return starShip[shipName] ?: "IDK"
+    }
+}
