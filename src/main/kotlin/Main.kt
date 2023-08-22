156707a20955020f6c9e531c1d0b93ff776d8053
CherifiMi <cherifi154949@gmail.com>
2023-08-22 05:51:35 +0100

    .

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
+import java.lang.RuntimeException
+
 fun main() {
 
 }
@@ -17,6 +19,18 @@ class StarShipLog: StarTrekRepo(){
     override fun getCaptin(shipName: String): String {
         return super.getCaptin(shipName)
         println(shipName)
+    }
+
+    override fun addCaptin(shipName: String, captinName: String) {
+        super.addCaptin(shipName, captinName)
+        println(shipName + captinName)
+    }
+}class StarShipCheck: StarTrekRepo(){
+    override fun getCaptin(shipName: String): String {
+        if (shipName.toList().size>15){
+            throw RuntimeException("tow long")
+        }
+        return super.getCaptin(shipName)
     }
 
     override fun addCaptin(shipName: String, captinName: String) {
