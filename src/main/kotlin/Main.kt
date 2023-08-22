2f6e85b253501d41b860836716b25b8d5198253d
CherifiMi <cherifi154949@gmail.com>
2023-08-22 06:15:29 +0100

    .

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,47 +1,7 @@
-import java.lang.RuntimeException
-
 fun main() {
 
-    val starTruk = DefultStarTrakRepo()
-    val withLog = StarShipLog(starTruk)
-    val withCheck = StarShipCheck(starTruk)
-    val withLogAndCheck = StarShipCheck(withLog)
-
-
-    println(starTruk == withCheck)
-
-}
-interface StarTrekRepo{
-    operator fun get(shipName: String): String
-    operator fun set(shipName: String, captinName: String)
 }
 
-class DefultStarTrakRepo: StarTrekRepo{
-
-    private val starShip = mutableMapOf("USS" to "Jean")
-
-    override fun get(shipName: String): String {
-        return starShip[shipName] ?: "IDK"
-    }
-
-    override fun set(shipName: String, captinName: String) {
-        starShip[shipName] = captinName
-    }
-}
-
-
-class StarShipLog(private val repo: StarTrekRepo): StarTrekRepo by repo{
-    override fun get(shipName: String): String {
-        println(shipName)
-        return repo[shipName]
-    }
-}
-
-class StarShipCheck(private val repo: StarTrekRepo): StarTrekRepo by repo{
-    override fun get(shipName: String): String {
-        if (shipName.toList().size>15){
-            throw RuntimeException("tow long")
-        }
-        return repo[shipName]
-    }
+interface USPlug{
+    val hasPower: Int
 }
