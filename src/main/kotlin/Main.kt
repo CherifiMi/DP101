a4fcfa7600a26284c7f9651b04a554dfac6298be
CherifiMi <cherifi154949@gmail.com>
2023-08-22 06:19:13 +0100

    .

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,3 +5,26 @@ fun main() {
 interface USPlug{
     val hasPower: Int
 }
+
+interface EUPlug{
+    val hasPower: Boolean
+}
+
+interface USBMine{
+    val hasPower: POWER
+}
+enum class POWER { TRUE,FALSE }
+
+interface UsbTypeC{
+    val hasPower: Boolean
+}
+
+
+
+fun cellPhone(chargeCable: UsbTypeC){
+    if (chargeCable.hasPower){
+        println("got power")
+    }else{
+        println("no power")
+    }
+}
