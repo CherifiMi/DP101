7eef95378fcab92aacaf0f02cf8223b21811c690
CherifiMi <cherifi154949@gmail.com>
2023-08-22 06:09:30 +0100

    .

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,42 +2,46 @@ import java.lang.RuntimeException
 
 fun main() {
 
-    val repo = DefultStarTrakRepo()
-    val repo2 = StarShipLog(repo)
-    val repo3 = StarShipCheck(repo2)
+    val starTruk = DefultStarTrakRepo()
+    val withLog = StarShipLog(starTruk)
+    val withCheck = StarShipCheck(starTruk)
+    val withLogAndCheck = StarShipCheck(withLog)
 
-    println( repo3.getCaptin("USS") )
+
+    println(withLogAndCheck["USS"])
+
 }
 interface StarTrekRepo{
-    fun getCaptin(shipName: String): String
-    fun addCaptin(shipName: String, captinName: String)
+    operator fun get(shipName: String): String
+    operator fun set(shipName: String, captinName: String)
 }
 
 class DefultStarTrakRepo: StarTrekRepo{
 
     private val starShip = mutableMapOf("USS" to "Jean")
 
-    override fun getCaptin(shipName: String): String{
+    override fun get(shipName: String): String {
         return starShip[shipName] ?: "IDK"
     }
-    override fun addCaptin(shipName: String, captinName: String){
+
+    override fun set(shipName: String, captinName: String) {
         starShip[shipName] = captinName
     }
 }
 
 
 class StarShipLog(private val repo: StarTrekRepo): StarTrekRepo by repo{
-    override fun getCaptin(shipName: String): String {
+    override fun get(shipName: String): String {
         println(shipName)
-        return repo.getCaptin(shipName)
+        return repo[shipName]
     }
 }
 
 class StarShipCheck(private val repo: StarTrekRepo): StarTrekRepo by repo{
-    override fun getCaptin(shipName: String): String {
+    override fun get(shipName: String): String {
         if (shipName.toList().size>15){
             throw RuntimeException("tow long")
         }
-        return repo.getCaptin(shipName)
+        return repo[shipName]
     }
 }
