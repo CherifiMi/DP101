c324ce2f7b135cffce8663fbb9be9c89ad0dc5db
CherifiMi <cherifi154949@gmail.com>
2023-08-22 06:32:28 +0100

    .

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,30 +1,61 @@
 fun main() {
 
+    cellPhone(
+        charger(
+            usPowerOutlit().toEUPlug()
+        ).toUsbTypeC()
+    )
+
 }
 
-interface USPlug{
+interface USPlug {
     val hasPower: Int
 }
 
-interface EUPlug{
-    val hasPower: Boolean
+interface EUPlug {
+    val hasPower: String
 }
 
-interface USBMine{
+interface USBMine {
     val hasPower: POWER
 }
-enum class POWER { TRUE,FALSE }
 
-interface UsbTypeC{
+enum class POWER { TRUE, FALSE }
+
+interface UsbTypeC {
     val hasPower: Boolean
 }
 
 
-
-fun cellPhone(chargeCable: UsbTypeC){
-    if (chargeCable.hasPower){
+fun cellPhone(chargeCable: UsbTypeC) {
+    if (chargeCable.hasPower) {
         println("got power")
-    }else{
+    } else {
         println("no power")
+    }
+}
+
+fun usPowerOutlit(): USPlug {
+    return object : USPlug {
+        override val hasPower: Int = 1
+    }
+}
+
+fun charger(plug: EUPlug): USBMine {
+    return object : USBMine {
+        override val hasPower: POWER = POWER.valueOf(plug.hasPower)
+    }
+}
+
+
+fun USBMine.toUsbTypeC(): UsbTypeC {
+    return object : UsbTypeC {
+        override val hasPower: Boolean = this@toUsbTypeC.hasPower == POWER.TRUE
+    }
+}
+
+fun USPlug.toEUPlug() : EUPlug{
+    return object : EUPlug{
+        override val hasPower: String = if (this@toEUPlug.hasPower == 1) "TRUE" else "FALSE"
     }
 }
